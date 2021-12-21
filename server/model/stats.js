30a36426913924ae8ea0ae311145cdac79a91260
Aayush <aayushmum@gmail.com>
2021-12-21 12:05:46 +0530

    changed stats model

--- server/model/stats.js
+++ server/model/stats.js
@@ -7,82 +7,86 @@ const statSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: true,
       },
-      ageGroupCount: [
+      data: [
         {
-          _11To18: {
-            type: Number,
-            default: 0,
+          date: {
+            type: String,
           },
-          _18To30: {
-            type: Number,
-            default: 0,
-          },
-          _31To59: {
-            type: Number,
-            default: 0,
-          },
-          _60AndAbove: {
-            type: Number,
-            default: 0,
-          },
-        },
-      ],
-      genderCount: [
-        {
-          male: {
-            type: Number,
-            default: 0,
-          },
-          female: {
-            type: Number,
-            default: 0,
-          },
-        },
-      ],
-      timestamp: [
-        {
-          _12pmTo3pm: {
-            type: Number,
-            default: 0,
-          },
-          _3pmTo6pm: {
-            type: Number,
-            default: 0,
-          },
-          _6pmTo9pm: {
-            type: Number,
-            default: 0,
-          },
-          _9pmTo12am: {
-            type: Number,
-            default: 0,
-          },
-          _12amTo3am: {
-            type: Number,
-            default: 0,
-          },
-          _3amTo6am: {
-            type: Number,
-            default: 0,
-          },
-          _6amTo9am: {
-            type: Number,
-            default: 0,
-          },
-          _9amTo12pm: {
-            type: Number,
-            default: 0,
-          },
-        },
-      ],
-      review: [
-        {
-          user: {
-            type: mongoose.Schema.Types.ObjectId,
-          },
-          rating: {
+          epoch: {
             type: Number,
           },
+          ageGroupCount: {
+            _11To18: {
+              type: Number,
+              default: 0,
+            },
+            _18To30: {
+              type: Number,
+              default: 0,
+            },
+            _31To59: {
+              type: Number,
+              default: 0,
+            },
+            _60AndAbove: {
+              type: Number,
+              default: 0,
+            },
+          },
+          genderCount: {
+            male: {
+              type: Number,
+              default: 0,
+            },
+            female: {
+              type: Number,
+              default: 0,
+            },
+          },
+          timestamp: {
+            _12pmTo3pm: {
+              type: Number,
+              default: 0,
+            },
+            _3pmTo6pm: {
+              type: Number,
+              default: 0,
+            },
+            _6pmTo9pm: {
+              type: Number,
+              default: 0,
+            },
+            _9pmTo12am: {
+              type: Number,
+              default: 0,
+            },
+            _12amTo3am: {
+              type: Number,
+              default: 0,
+            },
+            _3amTo6am: {
+              type: Number,
+              default: 0,
+            },
+            _6amTo9am: {
+              type: Number,
+              default: 0,
+            },
+            _9amTo12pm: {
+              type: Number,
+              default: 0,
+            },
+          },
+          review: [
+            {
+              user: {
+                type: mongoose.Schema.Types.ObjectId,
+              },
+              rating: {
+                type: Number,
+              },
+            },
+          ],
         },
       ],
     },
