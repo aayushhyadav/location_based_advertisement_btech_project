32eeec00cbb22c5f2fcf96d443270dc9be5e9564
Aayush <aayushmum@gmail.com>
2022-01-26 17:29:29 +0530

    added density epsilon mapping method

--- server/utility/densityToEpsilonMap.js
+++ server/utility/densityToEpsilonMap.js
@@ -10,13 +10,25 @@ const calDensity = (cluster) => {
       (cluster.clusters[i].clusterInd.length / val) * Math.PI * 4 * 10e4
 
     /*
-     * mapping [0, 1] to [0.25, 0.5]
+     * mapping [0, 1] to [0.2, 0.5]
      * former range is the probability of guessing the correct store visited by the user
      * for example business density = 4, then P(correctly guessing) = 0.25
      * latter range is minimum probability of making an error
      * so if P(guessing) is low then minimum probability of making an error can be on the lower side
      */
-    epsilon.push(((1 / Math.ceil(density)) * 0.25) / 1 + 0.25)
+    const probMinError = ((1 / Math.ceil(density)) * 0.3) / 1 + 0.2
+
+    const e =
+      (1 / (2 * Math.sqrt(area[i] / Math.PI))) *
+      Math.log((1 - probMinError) / probMinError)
+
+    if (e == 0) {
+      epsilon.push(0.004)
+    } else if (e > 0.1) {
+      epsilon.push(0.1)
+    } else {
+      epsilon.push(e)
+    }
   }
   return epsilon
 }
