cc2456ccb1be2fdb8e3158461230fb50b1c1c8fa
Aayush <aayushmum@gmail.com>
2022-01-30 13:51:19 +0530

    changes in mapping method

--- server/utility/densityToEpsilonMap.js
+++ server/utility/densityToEpsilonMap.js
@@ -1,34 +1,23 @@
 const calDensity = (cluster) => {
   const area = cluster.clusterSize
-  const compactness = []
   const epsilon = []
 
-  /* calculating business density in a region with radius = 200m */
   for (var i = 0; i < area.length; i++) {
-    val = area[i] * 10e6
-    const density =
-      (cluster.clusters[i].clusterInd.length / val) * Math.PI * 4 * 10e4
-
     /*
-     * mapping [0, 1] to [0.2, 0.5]
+     * mapping [0, 1] to [0.1, 0.5]
      * former range is the probability of guessing the correct store visited by the user
-     * for example business density = 4, then P(correctly guessing) = 0.25
+     * for example num ber of businesses = 4, then P(correctly guessing) = 0.25
      * latter range is minimum probability of making an error
      * so if P(guessing) is low then minimum probability of making an error can be on the lower side
      */
-    const probMinError = ((1 / Math.ceil(density)) * 0.3) / 1 + 0.2
+    const probMinError =
+      ((1 / cluster.clusters[i].clusterInd.length) * 0.4) / 1 + 0.1
 
     const e =
-      (1 / (2 * Math.sqrt(area[i] / Math.PI))) *
+      (1 / (1000 * Math.sqrt(area[i] / Math.PI))) *
       Math.log((1 - probMinError) / probMinError)
 
-    if (e == 0) {
-      epsilon.push(0.004)
-    } else if (e > 0.1) {
-      epsilon.push(0.1)
-    } else {
-      epsilon.push(e)
-    }
+    epsilon.push(e)
   }
   return epsilon
 }
