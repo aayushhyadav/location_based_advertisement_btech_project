7792848f47f100031faa86042da01fffb935979e
Aayush <aayushmum@gmail.com>
2023-08-01 08:17:18 +0530

    ui enhancements for dashboard

--- lba-frontend/screen/DashboardScreen.js
+++ lba-frontend/screen/DashboardScreen.js
@@ -3,10 +3,15 @@ import {
   StyleSheet,
   Text,
   View,
-  Image,
   TouchableOpacity,
   FlatList,
+  ScrollView,
 } from "react-native"
+import {Card, Image} from "@rneui/themed"
+
+const businessCardBg = require("../assets/business_card_bg.jpg")
+const exploreCardBg = require("../assets/explore_card_bg.jpg")
+const viewStoresCardBg = require("../assets/view_stores_card_bg.jpg")
 
 const DashboardScreen = ({navigation}) => {
   const initialState = {
@@ -15,23 +20,18 @@ const DashboardScreen = ({navigation}) => {
     data: [
       {
         id: 1,
-        name: "My Profile",
-        image: "https://img.icons8.com/clouds/100/000000/employee-card.png",
+        name: "View Stores",
+        image: viewStoresCardBg,
       },
       {
         id: 2,
-        name: "View Stores",
-        image: "https://img.icons8.com/color/100/000000/real-estate.png",
+        name: "Register Business",
+        image: businessCardBg,
       },
       {
         id: 3,
-        name: "Register Business",
-        image: "https://img.icons8.com/color/100/000000/real-estate.png",
-      },
-      {
-        id: 4,
         name: "Explore",
-        image: "https://img.icons8.com/dusk/70/000000/globe-earth.png",
+        image: exploreCardBg,
       },
     ],
   }
@@ -39,41 +39,33 @@ const DashboardScreen = ({navigation}) => {
   const [state, setState] = React.useState(initialState)
 
   const clickEventListener = (item) => {
-    if (item.id == 3) {
+    if (item.id == 2) {
       navigation.navigate("RegisterBusiness")
-    } else if (item.id == 2) {
+    } else if (item.id == 1) {
       navigation.navigate("ViewBusiness")
-    } else if (item.id == 4) {
+    } else if (item.id == 3) {
       navigation.navigate("Explore")
     }
   }
 
   return (
     <View style={styles.container}>
-      <FlatList
-        style={styles.contentList}
-        columnWrapperStyle={styles.listContainer}
-        data={state.data}
-        keyExtractor={(item) => {
-          return item.id
-        }}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity
-              style={styles.card}
-              onPress={() => {
-                clickEventListener(item)
-              }}
-            >
-              <Image style={styles.image} source={{uri: item.image}} />
-              <View style={styles.cardContent}>
-                <Text style={styles.name}>{item.name}</Text>
-                <Text style={styles.count}>{item.count}</Text>
-              </View>
-            </TouchableOpacity>
-          )
-        }}
-      />
+      <ScrollView>
+        {state.data.map((obj, index) => (
+          <TouchableOpacity
+            onPress={() => {
+              clickEventListener(obj)
+            }}
+            key={index}
+          >
+            <Card containerStyle={styles.cardContainer}>
+              <Card.Title>{obj.name}</Card.Title>
+              <Card.Divider />
+              <Image style={styles.image} source={obj.image} />
+            </Card>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   )
 }
@@ -86,68 +78,13 @@ const styles = StyleSheet.create({
     marginTop: 20,
     backgroundColor: "#ebf0f7",
   },
-  contentList: {
-    flex: 1,
-  },
-  cardContent: {
-    marginLeft: 20,
-    marginTop: 10,
-  },
   image: {
-    width: 90,
-    height: 90,
-    borderRadius: 45,
-    borderWidth: 2,
-    borderColor: "#ebf0f7",
+    width: 350,
+    height: 200,
+    resizeMode: "stretch",
   },
-
-  card: {
-    shadowColor: "#00000021",
-    shadowOffset: {
-      width: 0,
-      height: 6,
-    },
-    shadowOpacity: 0.37,
-    shadowRadius: 7.49,
-    elevation: 12,
-
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 20,
-    backgroundColor: "white",
-    padding: 10,
-    flexDirection: "row",
-    borderRadius: 30,
-  },
-
-  name: {
-    fontSize: 18,
-    flex: 1,
-    alignSelf: "center",
-    color: "#3399ff",
-    fontWeight: "bold",
-  },
-  count: {
-    fontSize: 14,
-    flex: 1,
-    alignSelf: "center",
-    color: "#6666ff",
-  },
-  followButton: {
-    marginTop: 10,
-    height: 35,
-    width: 100,
-    padding: 10,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 30,
-    backgroundColor: "white",
-    borderWidth: 1,
-    borderColor: "#dcdcdc",
-  },
-  followButtonText: {
-    color: "#000000",
-    fontSize: 12,
+  cardContainer: {
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
   },
 })
